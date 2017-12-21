ff91ea0267beafea20545e2aed2f590b681334d0
Tetsufumi Takeshita <takeshita.tetsufumi.44z@kyoto-u.jp>
2017-12-21 15:49:37 +0900

    First commit.

---
+++ search.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+<title>LexiconGrammaticumGraeco-Iaponicum</title>
+</head>
+<body>
+
+<h1>Lexicon Grammaticum Graeco-Iaponicum ギリシア語文法用語辞典</h1>
+
+<h2>検索</h2>
+<form method="POST" action="#">
+<input type="text" size="15">
+<input type="submit" value="検索">
+</form>
+
+</body>
+</html>
