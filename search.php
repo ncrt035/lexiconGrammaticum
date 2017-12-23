723dcfa815920bccdb65c1ae0fe0f9d8a2dfc966
Tetsufumi Takeshita <takeshita.tetsufumi.44z@kyoto-u.jp>
2017-12-23 14:00:09 +0900

    Edited search.php
    Database table(column name etc.) is not yet defined...

--- search.php
+++ search.php
@@ -1,5 +1,7 @@
 <?php
 require_once 'encode.php';
+require_once 'betacode2greek.php';
+require_once 'dbManager.php';
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,11 +15,35 @@ require_once 'encode.php';
 
 <h2>検索</h2>
 
-<form method="POST" action="#">
+<form method="POST" action="search.php">
   <label for="keyword">検索文字列：</label>
   <input id="keyword" type="text" name="keyword" size="15">
   <input type="submit" value="検索">
 </form>
+
+<?php
+//検索時の処理
+if (isset($_POST['keyword']) && !empty($_POST['keyword'])) {
+
+  try {
+    $db = getDb();
+
+    $stt = $db->prepare('SELECT * FROM greek WHERE vox LIKE :keyword ORDER BY vox ASC');
+    $stt->bindValue(':keyword', $_POST['keyword'].'%');
+
+    $stt->execute();//Execute SQL
+    $result = $stt->fetchAll(PDO::FETCH_ASSOC);//$resultに検索結果を多次元配列として格納
+    print_r($result);
+
+
+
+  } catch (PDOException $e) {
+    print "Error: {$e->getMessage()}";
+  }
+
+
+}
+?>
 
 </body>
 </html>
