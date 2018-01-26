01d82de1670c6ce1707b7cee7d389ebcbab25f77
Tetsufumi Takeshita <takeshita.tetsufumi.44z@kyoto-u.jp>
2018-01-26 12:24:57 +0900

    Shorten the names of variants, and add English menu

--- search.php
+++ search.php
@@ -14,32 +14,36 @@ require_once 'dbManager.php';
 
 <h1>Lexicon Grammaticum Graeco-Iaponicum ギリシア語文法用語辞典</h1>
 
-<h2>検索</h2>
+<h2>検索（Search）</h2>
 
 <form method="GET" action="search.php">
   <p>
-    <select id="option" name="option">
-      <option value="start">前方一致</option>
-      <option value="end">後方一致</option>
-      <option value="contain">部分一致</option>
-      <option value="exact">完全一致</option>
+    <select id="opt" name="opt">
+      <option value="start">前方一致（words starting with）</option>
+      <option value="end">後方一致（words ending with）</option>
+      <option value="contain">部分一致（words containing）</option>
+      <option value="exact">完全一致（the exact word）</option>
     </select>
-  </p>
-  <p>
-    <select id="field" name="field">
-      <option value="Word">見出し語</option>
-      <option value="Latin">ラテン語</option>
-      <option value="Expl">語義</option>
+
+    <select id="fld" name="fld">
+      <option value="Word">見出し語（in lemma）</option>
+      <option value="Latin">ラテン語（in latin）</option>
+      <option value="Expl">語義（in explanation）</option>
     </select>
   </p>
 
   <p>
-  <label for="keyword">検索文字列：</label>
-  <input id="keyword" type="text" name="keyword" size="15">
-    <input type="hidden" id="page" name="page" value="0">
+  <label for="kw">検索文字列（keyword）：</label>
+  <input id="kw" type="text" name="kw" size="15">
+  <input type="hidden" id="page" name="page" value="0">
   <input type="submit" value="検索">
   </p>
 </form>
+
+<p>
+  ギリシア語はBeta Codeでの入力も可能．<br>
+  You can also enter the Greek with Latin letters using Beta Code.
+</p>
 
 <?php
 
@@ -47,21 +51,21 @@ require_once 'dbManager.php';
 
   $vld = new checkInput();//入力値検証
 
-  $vld->requiredCheck($_GET['keyword'], '検索文字列');
-  $vld->arrayCheck($_GET['option'], '検索オプション', ['start','end','contain','exact']);
-  $vld->arrayCheck($_GET['field'], '検索領域', ['Word','Latin','Expl']);
+  $vld->requiredCheck($_GET['kw'], '検索文字列');
+  $vld->arrayCheck($_GET['opt'], '検索オプション', ['start','end','contain','exact']);
+  $vld->arrayCheck($_GET['fld'], '検索領域', ['Word','Latin','Expl']);
   $vld->pageCheck($_GET['page'], 'ページ数');
 
   $vld();//invoke
 
 
-  $option = htmlEnc($_GET['option']);
-  $field = htmlEnc($_GET['field']);
+  $option = htmlEnc($_GET['opt']);
+  $field = htmlEnc($_GET['fld']);
   $page = htmlEnc($_GET['page']);
   if ($field === 'Word') {
-    $keyword = htmlEnc(betacode2greek($_GET['keyword']));//replace betacode with greek letters
+    $keyword = htmlEnc(betacode2greek($_GET['kw']));//replace betacode with greek letters
     }else {
-      $keyword = htmlEnc($_GET['keyword']);
+      $keyword = htmlEnc($_GET['kw']);
   }
 
 
@@ -108,17 +112,16 @@ require_once 'dbManager.php';
 
     $total = count($result);
 
-      if ($page < ($total / MAX)) {//if $page is sound
+    if ($page <= ($total / MAX)) {//check whether $page is sound or not
         $count = $page * MAX;
-      }
-      else {
-        print 'ページ入力が不正です．';
+    }else {
+        print 'ページ数が不正です．';
         die();
-      }
+    }
 
 ?>
 
-<h3>検索結果：<?=$keyword?></h3>
+<h3>検索結果：<?=$keyword?> <?=$total?>件ヒット</h3>
 
   <ul>
     <?php
@@ -126,7 +129,9 @@ require_once 'dbManager.php';
       do {//結果のうちMAX個を出力 剰余で出力回数を制御するので条件を後置判定するdo...while文を用いる
         if (empty($result[$count]['Word'])){break;}
     ?>
+
       <li><b><?=$result[$count]['Word']?></b> <?=$result[$count]['Ew']?>: <i><?=$result[$count]['Latin']?></i> <?=$result[$count]['Expl']?></li>
+
     <?php
         $count++;
       } while ($count % MAX !== 0);
@@ -137,7 +142,7 @@ require_once 'dbManager.php';
   ?>
     <!--リンクをクリックすると次の検索結果を規定件数(=MAX)表示-->
     <!--生成したページ番号をpageという名前のGET情報として渡す-->
-    <a href="search.php?option=<?=$option?>&field=<?=$field?>&keyword=<?=$keyword?>&page=<?=$i?>">page<?=$i+1?></a>
+    <a href="search.php?opt=<?=$option?>&fld=<?=$field?>&kw=<?=$keyword?>&page=<?=$i?>">page<?=$i+1?></a>
 
   <?php
     }
